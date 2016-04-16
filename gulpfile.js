b2b2a3f3670c842465f5a682851e795224eaaf76
joraz89 <jramon.roblero@gmail.com>
2016-04-15 20:46:11 -0500

    Was uploaded repository

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    rollup = require('gulp-rollup'),
+    babel = require('gulp-babel'),
+    inject = require('gulp-inject'),
+    series = require('stream-series'),
+    sync = require('browser-sync'),
+    rollPaths = require('rollup-plugin-includepaths')
+
+gulp.task('js', function() {
+    var appJS = gulp.src('./app/*.js')
+        .pipe(rollup({
+            plugins: [rollPaths()]
+        }))
+        .pipe(gulp.dest('./dist'))
+    return gulp.src('index.html')
+        .pipe(inject(series(appJS), {
+            name: 'bundle',
+            relative: true
+        }))
+        .pipe(gulp.dest('./dist'))
+})
+gulp.task('server', function() {
+    sync({
+        server: './dist'
+    })
+})
+gulp.task('default', ['js', 'server'], function() {
+
+})
